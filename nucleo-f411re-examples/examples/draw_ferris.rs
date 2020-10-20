e6223e8ac7b2cff3519993495c8cd01efbc29c10
YruamaLairba <Yruama_Lairba@hotmail.com>
2020-10-21 00:16:39 +0200

    nucleo f411re example

---
+++ nucleo-f411re-examples/examples/draw_ferris.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate stm32f4xx_hal as hal;
+
+use cortex_m_rt::ExceptionFrame;
+use cortex_m_rt::{entry, exception};
+use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::rectangle::Rectangle;
+use embedded_graphics::style::PrimitiveStyleBuilder;
+
+use hal::delay::Delay;
+use hal::prelude::*;
+use hal::spi::{Mode, Phase, Polarity, Spi};
+use hal::stm32;
+use st7735_lcd::Orientation;
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
+
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(84.mhz()).pclk1(42.mhz()).freeze();
+    let gpioa = dp.GPIOA.split();
+    //let mut gpiob = dp.GPIOB.split();
+
+    // SPI1
+    let sck = gpioa.pa5.into_alternate_af5();
+    let miso = gpioa.pa6.into_alternate_af5();
+    let mosi = gpioa.pa7.into_alternate_af5();
+
+    let rst = gpioa.pa1.into_push_pull_output();
+    let dc = gpioa.pa0.into_push_pull_output();
+
+    let spi = Spi::spi1(
+        dp.SPI1,
+        (sck, miso, mosi),
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        16.mhz().into(),
+        clocks,
+    );
+
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let mut disp = st7735_lcd::ST7735::new(spi, dc, rst, true, false, 160, 128);
+
+    disp.init(&mut delay).unwrap();
+    disp.set_orientation(&Orientation::Landscape).unwrap();
+    let style = PrimitiveStyleBuilder::new()
+        .fill_color(Rgb565::BLACK)
+        .build();
+    let black_backdrop = Rectangle::new(Point::new(0, 0), Point::new(160, 128)).into_styled(style);
+    black_backdrop.draw(&mut disp).unwrap();
+
+    disp.set_offset(0, 25);
+
+    // draw ferris
+    let image_raw: ImageRawLE<Rgb565> =
+        ImageRaw::new(include_bytes!("../../assets/ferris.raw"), 86, 64);
+    let image: Image<_, Rgb565> = Image::new(&image_raw, Point::new(34, 8));
+    image.draw(&mut disp).unwrap();
+
+    loop {
+        continue;
+    }
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
+}
